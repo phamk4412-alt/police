9f5667d676280c6e9c3b2c66cbd93012d68bc8c7
Mexirish Hiroyuki <lhnh29303@gmail.com>
2026-04-20 15:38:15 +0700

    Update main with current workspace snapshot

--- webserver/start-server.ps1
+++
@@ -1,4 +0,0 @@
-$env:DOTNET_CLI_HOME = "D:\police\.dotnet"
-$env:DOTNET_SKIP_FIRST_TIME_EXPERIENCE = "1"
-
-dotnet run --project "D:\police\webserver\PoliceWebServer.csproj" --urls "http://localhost:5055"
